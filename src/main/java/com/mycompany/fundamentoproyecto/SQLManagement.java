934edc79cbf923850be0024618cdedd1227a3446
unknown <marcelatovar.flores@gmail.com>
2025-05-19 20:51:25 -0600

    primer commit

---
+++ src/main/java/com/mycompany/fundamentoproyecto/SQLManagement.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fundamentoproyecto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class SQLManagement {
+    
+    
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=master";
+    private static final String USER = "marcela";
+    private static final String PASSWORD = "sqlserver";
+
+    public Connection conect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return conn;
+    }
+}
+
